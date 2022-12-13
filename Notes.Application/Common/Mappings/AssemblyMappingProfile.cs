221f63a1f96ec19519082291693ddea27b0e4b4d
Volodymyr Moiseichenko <alternativeqoob@gmail.com>
2022-12-13 20:54:35 +0200

    Add project files.

---
+++ Notes.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace Notes.Application.Common.Mappings
+{
+    internal class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingFromAssembly(assembly);
+
+        private void ApplyMappingFromAssembly(Assembly assembly) // realisation of mapping
+        {                                                        // method scans assembly and searches 
+            {                                                    // all types that implements IMapWith interface   
+                var types = assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                    .Any(i => i.IsGenericType &&
+                    i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+
+                foreach (var type in types)
+                {
+                    var instance = Activator.CreateInstance(type);
+                    var methodInfo = type.GetMethod("Mapping");
+                    methodInfo?.Invoke(instance, new object[] { this });
+                }
+            }
+        }
+    }
+}
---
+++ Notes.Persistance/EntityTypeConfigurations/NoteConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+
+namespace Notes.Persistence.EntityTypeConfigurations
+{
+    public class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            builder.HasKey(note => note.Id); // Id is our key
+            builder.HasIndex(note => note.Id).IsUnique(); // id key is unique
+            builder.Property(note => note.Title).HasMaxLength(250); // Title is no longer than 250 symbols in length
+        }
+    }
+}
