34ce7bc0fd2d1de71cd338837c982645c16c8cda
Volodymyr Moiseichenko <alternativeqoob@gmail.com>
2022-12-14 23:35:05 +0200

    Modifying Program.cs in WebApi

--- Notes.WebAPI/Program.cs
+++ Notes.WebAPI/Program.cs
@@ -1,3 +1,5 @@
+using Notes.Persistence;
+
 namespace Notes.WebAPI
 {
     public class Program
@@ -5,11 +7,26 @@ namespace Notes.WebAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            using (var scope = builder.Services.BuildServiceProvider().CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                try
+                {
+                    var context = serviceProvider.GetRequiredService<NotesDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch(Exception e) { }
+            }
+                
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            //app.MapGet("/", () => "Hello World!");
 
             app.Run();
         }
+
+        
+        
     }
 }
