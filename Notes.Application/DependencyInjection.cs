efb7bd2bfbf81bd9ccedd7b43b7b4ad0f4204351
Volodymyr Moiseichenko <alternativeqoob@gmail.com>
2022-12-14 20:49:07 +0200

    Adding DI

---
+++ Notes.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using System.Reflection;
+
+namespace Notes.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
