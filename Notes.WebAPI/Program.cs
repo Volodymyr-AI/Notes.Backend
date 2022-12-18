5c5d1b39d1cfbe378ca4c4022619ad0feb8ed155
Volodymyr Moiseichenko <alternativeqoob@gmail.com>
2022-12-18 22:03:59 +0200

    Created Controllers

--- Notes.WebAPI/Program.cs
+++ Notes.WebAPI/Program.cs
@@ -10,17 +10,52 @@ namespace Notes.WebAPI
 {
     public class Program
     {
+        
         public static void Main(string[] args)
         {
             
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddAplication();
+            using (var scope = builder.Services.BuildServiceProvider().CreateScope()) // invoke method of Db initialization
+            {
+                var serviceProvider = scope.ServiceProvider;
+                try
+                {
+                    var context = serviceProvider.GetRequiredService<NotesDbContext>(); // for accessing dependencies
+                    DbInitializer.Initialize(context); // initialize database
+                }
+                catch (Exception e) { }
+            }
+            builder.Services.AddAutoMapper(config => // adding automapper to program
+            {
+                config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+                config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+            });
 
-            builder.Services.AddAutoMapper(typeof(Note));
+
+            builder.Services.AddAplication();
+            builder.Services.AddControllers();
+
+            //IConfiguration Configuration;
+            //builder.Services.AddPersistence(Configuration);
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyHeader();
+                    policy.AllowAnyMethod();
+                    policy.AllowAnyOrigin();
+                });
+            });
 
             var app = builder.Build();
-
+            app.UseHttpsRedirection();
+            app.UseCors("AllowAll");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
             //app.MapGet("/", () => "Hello World!");
             
             app.Run();
