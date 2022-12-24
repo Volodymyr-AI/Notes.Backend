0482d39e622ab3f65e92e091f7652d36af69ce3c
Volodymyr Moiseichenko <alternativeqoob@gmail.com>
2022-12-24 20:06:55 +0200

    Fixed problem with Handle method in ValidationBehavior class

--- Notes.Application/Common/Behaviors/ValidationBehavior.cs
+++ Notes.Application/Common/Behaviors/ValidationBehavior.cs
@@ -18,16 +18,16 @@ namespace Notes.Application.Common.Behaviors
         { 
             _validators = validators; 
         }
-        public Task<TResponse> Handle(TRequest request,
-            CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
             var context = new ValidationContext<TRequest>(request);
             var failures = _validators
                 .Select(v => v.Validate(context))
                 .SelectMany(result => result.Errors)
-                .Where(failure => failure!=null)
+                .Where(failure => failure != null)
                 .ToList();
-            if(failures.Count!=0)
+            if (failures.Count != 0)
             {
                 throw new ValidationException(failures);
             }
